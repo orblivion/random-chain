d2ca3e62647e29c5d20b1d51247db978d3feb0a6
Daniel Krol <orblivion@gmail.com>
2015-01-14 12:00:39 -0800

    List all files in training directory

--- Main.hs
+++ Main.hs
@@ -1,1 +1,7 @@
-main = return ()
+import qualified System.Directory as Directory
+import qualified System.FilePath.Posix as Posix
+
+main = do
+  fnames <- Directory.getDirectoryContents "training"
+  let desiredFnames = filter ((== ".txt") . Posix.takeExtension) fnames
+  mapM putStrLn desiredFnames
