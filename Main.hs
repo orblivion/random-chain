9e8c939c2493db8579be5b7190aa53c752481760
Daniel Krol <orblivion@gmail.com>
2015-01-14 13:54:49 -0800

    Training word getting, chainset buliding

--- Main.hs
+++ Main.hs
@@ -1,19 +1,47 @@
 import qualified System.Directory as Directory
 import qualified System.FilePath.Posix as Posix
 import qualified Data.Map as DataMap
+import qualified Data.Text as DataText
+import qualified Data.Maybe as Maybe
 
-data TrainingWord = TrainingWord String deriving Show
+data TrainingWord = TrainingWord String deriving (Show, Eq, Ord)
 data TrainingText = TrainingText [TrainingWord] deriving Show
-data Chain = ChainEnd | Chain (DataMap.Map TrainingWord Chain) deriving Show
 
-getNextChain :: TrainingText -> Int -> (Maybe Chain, TrainingText)
-getNextChain tText chainLength = undefined
+data Chain = Chain [TrainingWord] deriving Show
+data ChainSet = ChainEnd | ChainSet (DataMap.Map TrainingWord ChainSet) deriving Show
+
+addChain :: ChainSet -> Chain -> ChainSet
+addChain chainSet (Chain []) = chainSet
+addChain (ChainSet csMap) (Chain chainWords) = ChainSet newCsMap where
+    newCsMap = (DataMap.insert chainHead newInnerChainSet csMap)
+    chainHead = head chainWords
+    chainRest = Chain $ tail chainWords
+    oldInnerChainSet = Maybe.fromMaybe ChainEnd $ DataMap.lookup chainHead csMap
+    newInnerChainSet = addChain oldInnerChainSet chainRest
+
+-- Creates a chain if it's long enough
+getChain :: TrainingText -> Int -> Maybe Chain
+getChain (TrainingText str) desiredLength
+    | actualLength == desiredLength = Just $ Chain chainWords
+    | otherwise = Nothing
+        where
+            chainWords = take desiredLength str
+            actualLength = length chainWords
+
+-- Given a training text, which could be a subset of another training text,
+-- assemble the next chainset
+getNextChainSet :: ChainSet -> TrainingText -> Int ->  ChainSet
+getNextChainSet chainSet tText chainLength = maybe chainSet (addChain chainSet) (getChain tText chainLength)
 
 getTrainingText :: String -> TrainingText
-getTrainingText = undefined
+getTrainingText trainingString = TrainingText $ getTrainingWords trainingString
     where
         getTrainingWords :: String -> [TrainingWord]
-        getTrainingWords = undefined
+        getTrainingWords trainingString = map (TrainingWord . cleanWord . DataText.unpack)
+            $ DataText.splitOn (DataText.pack " ")
+            $ DataText.pack trainingString
+        cleanWord :: String -> String
+        cleanWord = id -- later we can remove commas, etc
 
 getTrainingFilenames = do
   fnames <- Directory.getDirectoryContents "training"
