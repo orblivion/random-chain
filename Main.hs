56ffb9b10e0c180e976255689ae1201fed672508
Daniel Krol <orblivion@gmail.com>
2015-01-14 15:09:32 -0800

    Fixes missing exhaustive search. Fixes some more warnings. Groomed output

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ import qualified System.FilePath.Posix as Posix
 import qualified Data.Map as DataMap
 import qualified Data.Text as DataText
 import qualified Data.Maybe as Maybe
+import qualified Text.Groom as Groom
 
 data TrainingWord = TrainingWord String deriving (Show, Eq, Ord)
 data TrainingText = TrainingText [TrainingWord] deriving Show
@@ -12,6 +13,7 @@ data ChainSet = ChainEnd | ChainSet (DataMap.Map TrainingWord ChainSet) deriving
 
 addChain :: ChainSet -> Chain -> ChainSet
 addChain chainSet (Chain []) = chainSet
+addChain ChainEnd chain = addChain (ChainSet (DataMap.fromList [])) chain
 addChain (ChainSet csMap) (Chain chainWords) = ChainSet newCsMap where
     newCsMap = (DataMap.insert chainHead newInnerChainSet csMap)
     chainHead = head chainWords
@@ -35,34 +37,37 @@ addTrainingTextToChainSet chainLength chainSet trainingText =
         where
             getSubTrainingTexts :: TrainingText -> [TrainingText]
             getSubTrainingTexts (TrainingText []) = []
-            getSubTrainingTexts (TrainingText words) =
-                (TrainingText words):(getSubTrainingTexts (TrainingText (tail words)))
+            getSubTrainingTexts (TrainingText tWords) =
+                (TrainingText tWords):(getSubTrainingTexts (TrainingText (tail tWords)))
 
             -- Given a training text, which could be a subset of another training text,
             -- assemble the next chainset
             getNextChainSet :: ChainSet -> TrainingText -> ChainSet
-            getNextChainSet chainSet tText =
-                maybe chainSet (addChain chainSet) (getChain tText chainLength)
+            getNextChainSet chainSet' tText =
+                maybe chainSet' (addChain chainSet') (getChain tText chainLength)
 
 getTrainingText :: String -> TrainingText
 getTrainingText trainingString = TrainingText $ getTrainingWords trainingString
     where
         getTrainingWords :: String -> [TrainingWord]
-        getTrainingWords trainingString = map (TrainingWord . cleanWord . DataText.unpack)
+        getTrainingWords trainingString' = map (TrainingWord . cleanWord . DataText.unpack)
             $ DataText.splitOn (DataText.pack " ")
-            $ DataText.pack trainingString
+            $ DataText.pack trainingString'
         cleanWord :: String -> String
         cleanWord = id -- later we can remove commas, etc
 
+getTrainingFilenames :: IO [String]
 getTrainingFilenames = do
   fnames <- Directory.getDirectoryContents "training"
   return $ map ("training/" ++) $ filter ((== ".txt") . Posix.takeExtension) fnames
 
+getTrainingStrings :: IO [String]
 getTrainingStrings = getTrainingFilenames >>= mapM readFile
 
+main :: IO ()
 main = do
     trainingStrings <- getTrainingStrings
     let trainingTexts = map getTrainingText trainingStrings
     let chainSet = foldl (addTrainingTextToChainSet 5) ChainEnd trainingTexts
-    putStrLn $ show chainSet
+    putStrLn $ Groom.groom chainSet
     return ()
